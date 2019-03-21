1d37d54f292b854b2a9691dd9318ae3757490b3e
Diego <ldiego900@gmail.com>
2019-03-20 21:04:32 -0400

    sdasad

--- js/app.js
+++ js/app.js
@@ -1,14 +1,46 @@
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyA3TFDsjaJEaA9QVyn1LyDPdbzkJ6kDzyY",
-    authDomain: "blood-1ea0c.firebaseapp.com",
-    databaseURL: "https://blood-1ea0c.firebaseio.com",
-    projectId: "blood-1ea0c",
-    storageBucket: "blood-1ea0c.appspot.com",
-    messagingSenderId: "961140727538"
-  };
-  firebase.initializeApp(config);
-
-const dbRef = firebase.database().ref();
-const usersRef = dbRef.child('usuarios');
+firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+  
+      document.getElementById("user_div").style.display = "block";
+      document.getElementById("login_div").style.display = "none";
+  
+      var user = firebase.auth().currentUser;
+  
+      if(user != null){
+  
+        var email_id = user.email;
+        document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+  
+      }
+  
+    } else {
+      // No user is signed in.
+  
+      document.getElementById("user_div").style.display = "none";
+      document.getElementById("login_div").style.display = "block";
+  
+    }
+  });
+  
+  function login(){
+  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+  
+    firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+  
+      window.alert("Error : " + errorMessage);
+  
+      // ...
+    });
+  
+  }
+  
+  function logout(){
+    firebase.auth().signOut();
+  }
+  
